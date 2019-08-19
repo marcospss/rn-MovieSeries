2ab350bd6858018c6cc265814478e043b8c47fc5
Marcos Silva Santos <marcos.silva.santos@accenture.com>
2019-08-19 19:16:09 -0300

    Ícones e navegação

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { 
   createAppContainer, 
   createStackNavigator,
@@ -8,103 +9,151 @@ import {
 } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import NavigationHelper from '~/helpers/Navigation';
 import Movies from '~/screens/Movies';
 import Series from '~/screens/Series';
 import Search from '~/screens/Search';
 import Favorites from '~/screens/Favorites';
 import { DetailsMovies, DetailsSeries } from '~/screens/Details';
+import logo from '~/assets/images/imdb-logo.png';
 
-const MoviesStack = createStackNavigator({
-  Movies: {
-    screen: Movies,
-    navigationOptions: {
-      header: null,
-    }
-  }
-});
-
-const SeriesStack = createStackNavigator({
-  Series: {
-    screen: Series,
-    navigationOptions: {
-      header: null,
-    }
-  }
-});
-
-const SearchStack = createStackNavigator({
-  Search: {
-    screen: Search,
-    navigationOptions: {
-      header: null,
-    }
-  }
-});
-
-const FavoritesStack = createStackNavigator({
-  Favorites: {
-    screen: Favorites,
-    navigationOptions: {
-      header: null,
-    }
-  }
-});
-
-const MainTabs = createBottomTabNavigator({
-  Movies: {
-    screen: MoviesStack,
-    navigationOptions: {
-      tabBarLabel: null,
-      tabBarIcon: ({ tintColor }) =>
-        <Icon name='film' size={24} color={tintColor} />
-    }
-  },
-  Series: {
-    screen: SeriesStack,
-    navigationOptions: {
-      tabBarLabel: null,
-      tabBarIcon: ({ tintColor }) =>
-        <Icon name='tv' size={24} color={tintColor} />
-    }
-  },
-  Search: {
-    screen: SearchStack,
-    navigationOptions: {
-      tabBarLabel: null,
-      tabBarIcon: ({ tintColor }) =>
-        <Icon name='search' size={24} color={tintColor} />
-    }
-  }
-},
-{
-  tabBarOptions: {
-    initialRouteName: 'Movies',
-    activeTintColor: '#fff',
-    showLabel: false,
-    style: {
-      backgroundColor: '#000',
+const defaultNavigation = {
+  headerLayoutPreset: 'center',
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerTitle: <Image source={logo} />,
+    headerStyle: {
+      backgroundColor: '#000'
     },
-  }
-});
+    headerLeft: (
+      <Icon 
+        style={{ paddingLeft: 10 }}
+        onPress={() => navigation.openDrawer()}
+        name="bars"
+        size={26}
+        color="#fff"
+      />
+    ),
+    headerRight: (
+      <Icon 
+        style={{ paddingLeft: 10 }}
+        onPress={() => NavigationHelper.navigate('Favorites')}
+        name="heart"
+        size={26}
+        color="#fff"
+      />
+    ),
+  })
+};
 
-const DetailsStack = createStackNavigator({
-  DetailsMovies: {
-    screen: DetailsMovies,
+const MoviesStack = createStackNavigator(
+  {
+    Movies: {
+      screen: Movies,
+    }
   },
-  DetailsSeries: {
-    screen: DetailsSeries,
-  }
-});
+  defaultNavigation
+);
 
-const MainDrawer = createDrawerNavigator({
-  Home: MainTabs,
-  Favorites: FavoritesStack
-});
+const SeriesStack = createStackNavigator(
+  {
+    Series: {
+      screen: Series,
+    }
+  },
+  defaultNavigation
+);
+
+const SearchStack = createStackNavigator(
+  {
+    Search: {
+      screen: Search,
+    }
+  },
+  defaultNavigation
+);
+
+const FavoritesStack = createStackNavigator(
+  {
+    Favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        header: null,
+      }
+    }
+  }
+);
+
+const MainTabs = createBottomTabNavigator(
+  {
+    Movies: {
+      screen: MoviesStack,
+      navigationOptions: {
+        tabBarLabel: null,
+        tabBarIcon: ({ tintColor }) =>
+          <Icon name='film' size={24} color={tintColor} />
+      }
+    },
+    Series: {
+      screen: SeriesStack,
+      navigationOptions: {
+        tabBarLabel: null,
+        tabBarIcon: ({ tintColor }) =>
+          <Icon name='tv' size={24} color={tintColor} />
+      }
+    },
+    Search: {
+      screen: SearchStack,
+      navigationOptions: {
+        tabBarLabel: null,
+        tabBarIcon: ({ tintColor }) =>
+          <Icon name='search' size={24} color={tintColor} />
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      initialRouteName: 'Movies',
+      activeTintColor: '#fff',
+      showLabel: false,
+      style: {
+        backgroundColor: '#000',
+      },
+    }
+  }
+);
+
+const DetailsStack = createStackNavigator(
+  {
+    DetailsMovies: {
+      screen: DetailsMovies,
+    },
+    DetailsSeries: {
+      screen: DetailsSeries,
+    }
+  }
+);
+
+const MainDrawer = createDrawerNavigator(
+  {
+    Home: {
+      screen: MainTabs,
+      navigationOptions: {
+        drawerIcon: (<Icon name='home' size={24} color="#000" />)
+      }
+    },
+    Favorites: {
+      screen: FavoritesStack,
+      navigationOptions: {
+        drawerIcon: (<Icon name='heart' size={24} color="#000" />)
+      }
+    }
+  }
+);
 
 const AppModalStack = createStackNavigator(
   {
     App: MainDrawer,
-    DetailsMovies: {
+    DetailsScreens: {
       screen: DetailsStack,
     }
   },
@@ -114,10 +163,12 @@ const AppModalStack = createStackNavigator(
   }
 );
 
-const App = createSwitchNavigator({
-  DrawerNavigator: {
-    screen: AppModalStack
-  }
-});
-
-export default createAppContainer(App);
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      DrawerNavigator: {
+        screen: AppModalStack
+      }
+    }
+  )
+);
