2741e0928b377759217d4c1b1d3d0e4e4fb7e56d
Marcos Silva Santos <marcos.silva.santos@accenture.com>
2019-11-25 19:23:19 -0300

    Correção imagens

--- src/config/Settings.js
+++ src/config/Settings.js
@@ -2,6 +2,6 @@ export default SETTINGS = {
     dbName: 'MovieDB',
     storeName: '@favorites_collection',
     apiEndpoint: 'https://api.themoviedb.org/3',
-    apikey: 'd272326e467344029e68e3c4ff0b4059',
+    apikey: '--YOUR-KEY--',
     language: 'en-US'
 };
